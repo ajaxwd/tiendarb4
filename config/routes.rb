df0cb0a6fafbd3d20f2943ad9f00ecb352e34b74
ajaxwd <ajaxwd@gmail.com>
2016-05-28 01:55:55 -0400

    navegacion del sitio

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Tienda::Application.routes.draw do
  
   resources :attachments, only: [:create,:destroy,:new,:show]
   resources :products
+  resources :in_shopping_carts, only: [:create,:destroy]
   devise_for :users
 
   post "/emails/create", as: :create_email
+
+  get "/carrito", to: "shopping_carts#show"
 
   authenticated :user do
     root 'welcome#index'
