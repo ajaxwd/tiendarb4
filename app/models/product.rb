0c784d4cfa3a4792d215c4b177e9dc5bba10609e
ajaxwd <ajaxwd@gmail.com>
2016-07-12 23:32:56 -0400

    mejoras en el dashboard

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,7 +19,10 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   has_many :attachments
-  
+  has_many :in_shopping_carts
+  has_one :shopping_cart, through: :in_shopping_carts
+  has_many :my_payments, through: :shopping_cart
+
   validates_presence_of :name,:user,:pricing
   validates :pricing, numericality: { greater_than: 0}
 
@@ -28,6 +31,14 @@ class Product < ActiveRecord::Base
 
   def paypal_from
   	{name: name, sku: :item, price: (pricing / 100), currency:"USD",quanity:1 }
+  end
+
+  def sales
+  	my_payments.payed
+  end
+
+  def sales_total
+  	my_payments.payed.count * self.pricing
   end
 
 end
